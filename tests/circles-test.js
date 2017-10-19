3f5723a772ebee9ae24668bf57fc3d0533c34dbd
Riccardo Scalco <riccardo.scalco@gmail.com>
2017-10-19 15:40:01 +0200

    some tests

--- tests/circles-test.js
+++ tests/circles-test.js
@@ -11,3 +11,21 @@ tape('svg.call(texture) append a defs node', t => {
 	t.ok(!svg.select('defs').empty());
 	t.end();
 })
+
+tape('svg.call(texture) append a pattern node', t => {
+	const texture = textures.circles();
+	const document = jsdom('<svg></svg>');
+	const svg = d3.select(document).select('svg');
+	svg.call(texture);
+	t.ok(!svg.select('defs').select('pattern').empty());
+	t.end();
+})
+
+tape('svg.call(texture) append a pattern node with the id attribute', t => {
+	const texture = textures.circles();
+	const document = jsdom('<svg></svg>');
+	const svg = d3.select(document).select('svg');
+	svg.call(texture);
+	t.ok(svg.select('defs').select('pattern').attr('id') instanceof 'String');
+	t.end();
+})
