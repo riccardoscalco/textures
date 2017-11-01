896bdc9c44b99afa8f955b0f8fc0fa5fe97fd250
Riccardo Scalco <riccardo.scalco@gmail.com>
2017-11-01 15:50:58 +0100

    add babel

--- dist/textures.esm.js
+++ dist/textures.esm.js
@@ -1,56 +1,30 @@
-var rand = () =>
-	`${Math.random().toString(36)}00000000000000000`
-		.replace(/[^a-z]+/g, '')
-		.slice(0, 5);
+var rand = (function () {
+	return (Math.random().toString(36) + '00000000000000000').replace(/[^a-z]+/g, '').slice(0, 5);
+});
 
-var circles = () => {
-	let size = 20;
-	let background = '';
-	let radius = 2;
-	let complement = false;
-	let fill = '#343434';
-	let stroke = '#343434';
-	let strokeWidth = 0;
-	let id = rand();
+var circles = (function () {
+	var size = 20;
+	var background = '';
+	var radius = 2;
+	var complement = false;
+	var fill = '#343434';
+	var stroke = '#343434';
+	var strokeWidth = 0;
+	var id = rand();
 
-	const $ = selection => {
-		const group = selection
-			.append('defs')
-			.append('pattern')
-				.attr('id', id)
-				.attr('patternUnits', 'userSpaceOnUse')
-				.attr('width', size)
-				.attr('height', size);
+	var $ = function $(selection) {
+		var group = selection.append('defs').append('pattern').attr('id', id).attr('patternUnits', 'userSpaceOnUse').attr('width', size).attr('height', size);
 
 		if (background) {
-			group
-				.append('rect')
-					.attr('width', size)
-					.attr('height', size)
-					.attr('fill', background);
+			group.append('rect').attr('width', size).attr('height', size).attr('fill', background);
 		}
 
-		group
-			.append('circle')
-				.attr('cx', size / 2)
-				.attr('cy', size / 2)
-				.attr('r', radius)
-				.attr('fill', fill)
-				.attr('stroke', stroke)
-				.attr('stroke-width', strokeWidth);
+		group.append('circle').attr('cx', size / 2).attr('cy', size / 2).attr('r', radius).attr('fill', fill).attr('stroke', stroke).attr('stroke-width', strokeWidth);
 
 		if (complement) {
-			[[0, 0], [0, size], [size, 0], [size, size]]
-				.forEach(corner => {
-					group
-						.append('circle')
-							.attr('cx', corner[0])
-							.attr('cy', corner[1])
-							.attr('r', radius)
-							.attr('fill', fill)
-							.attr('stroke', stroke)
-							.attr('stroke-width', strokeWidth);
-				});
+			[[0, 0], [0, size], [size, 0], [size, size]].forEach(function (corner) {
+				group.append('circle').attr('cx', corner[0]).attr('cy', corner[1]).attr('r', radius).attr('fill', fill).attr('stroke', stroke).attr('stroke-width', strokeWidth);
+			});
 		}
 	};
 
@@ -138,75 +112,58 @@ var circles = () => {
 	};
 
 	$.url = function () {
-		return `url(#${id})`;
+		return 'url(#' + id + ')';
 	};
 
 	return $;
-};
+});
 
-var lines = () => {
-	let size = 20;
-	let stroke = '#343434';
-	let strokeWidth = 2;
-	let background = '';
-	let id = rand();
-	let orientation = ['diagonal'];
-	let shapeRendering = 'auto';
+var lines = (function () {
+	var size = 20;
+	var stroke = '#343434';
+	var strokeWidth = 2;
+	var background = '';
+	var id = rand();
+	var orientation = ['diagonal'];
+	var shapeRendering = 'auto';
 
-	const path = orientation => {
-		const s = size;
+	var path = function path(orientation) {
+		var s = size;
 		switch (orientation) {
 			case '0/8':
 			case 'vertical':
-				return `M ${s / 2}, 0 l 0, ${s}`;
+				return 'M ' + s / 2 + ', 0 l 0, ' + s;
 			case '1/8':
-				return `M ${s / 4},0 l ${s / 2},${s} M ${-s / 4},0 l ${s / 2},${s} M ${s * 3 / 4},0 l ${s / 2},${s}`;
+				return 'M ' + s / 4 + ',0 l ' + s / 2 + ',' + s + ' M ' + -s / 4 + ',0 l ' + s / 2 + ',' + s + ' M ' + s * 3 / 4 + ',0 l ' + s / 2 + ',' + s;
 			case '2/8':
 			case 'diagonal':
-				return `M 0,${s} l ${s},${-s} M ${-s / 4},${s / 4} l ${s / 2},${-s / 2} M ${3 / 4 * s},${5 / 4 * s} l ${s / 2},${-s / 2}`;
+				return 'M 0,' + s + ' l ' + s + ',' + -s + ' M ' + -s / 4 + ',' + s / 4 + ' l ' + s / 2 + ',' + -s / 2 + ' M ' + 3 / 4 * s + ',' + 5 / 4 * s + ' l ' + s / 2 + ',' + -s / 2;
 			case '3/8':
-				return `M 0,${3 / 4 * s} l ${s},${-s / 2} M 0,${s / 4} l ${s},${-s / 2} M 0,${s * 5 / 4} l ${s},${-s / 2}`;
+				return 'M 0,' + 3 / 4 * s + ' l ' + s + ',' + -s / 2 + ' M 0,' + s / 4 + ' l ' + s + ',' + -s / 2 + ' M 0,' + s * 5 / 4 + ' l ' + s + ',' + -s / 2;
 			case '4/8':
 			case 'horizontal':
-				return `M 0,${s / 2} l ${s},0`;
+				return 'M 0,' + s / 2 + ' l ' + s + ',0';
 			case '5/8':
-				return `M 0,${-s / 4} l ${s},${s / 2}M 0,${s / 4} l ${s},${s / 2} M 0,${s * 3 / 4} l ${s},${s / 2}`;
+				return 'M 0,' + -s / 4 + ' l ' + s + ',' + s / 2 + 'M 0,' + s / 4 + ' l ' + s + ',' + s / 2 + ' M 0,' + s * 3 / 4 + ' l ' + s + ',' + s / 2;
 			case '6/8':
-				return `M 0,0 l ${s},${s} M ${-s / 4},${3 / 4 * s} l ${s / 2},${s / 2} M ${s * 3 / 4},${-s / 4} l ${s / 2},${s / 2}`;
+				return 'M 0,0 l ' + s + ',' + s + ' M ' + -s / 4 + ',' + 3 / 4 * s + ' l ' + s / 2 + ',' + s / 2 + ' M ' + s * 3 / 4 + ',' + -s / 4 + ' l ' + s / 2 + ',' + s / 2;
 			case '7/8':
-				return `M ${-s / 4},0 l ${s / 2},${s} M ${s / 4},0 l ${s / 2},${s} M ${s * 3 / 4},0 l ${s / 2},${s}`;
+				return 'M ' + -s / 4 + ',0 l ' + s / 2 + ',' + s + ' M ' + s / 4 + ',0 l ' + s / 2 + ',' + s + ' M ' + s * 3 / 4 + ',0 l ' + s / 2 + ',' + s;
 			default:
-				return `M ${s / 2}, 0 l 0, ${s}`;
+				return 'M ' + s / 2 + ', 0 l 0, ' + s;
 		}
 	};
 
-	const $ = selection => {
-		const group = selection
-			.append('defs')
-			.append('pattern')
-				.attr('id', id)
-				.attr('patternUnits', 'userSpaceOnUse')
-				.attr('width', size)
-				.attr('height', size);
+	var $ = function $(selection) {
+		var group = selection.append('defs').append('pattern').attr('id', id).attr('patternUnits', 'userSpaceOnUse').attr('width', size).attr('height', size);
 
 		if (background) {
-			group
-				.append('rect')
-					.attr('width', size)
-					.attr('height', size)
-					.attr('fill', background);
+			group.append('rect').attr('width', size).attr('height', size).attr('fill', background);
 		}
 
-		orientation
-			.forEach(o => {
-				group
-					.append('path')
-						.attr('d', path(o))
-						.attr('stroke-width', strokeWidth)
-						.attr('shape-rendering', shapeRendering)
-						.attr('stroke', stroke)
-						.attr('stroke-linecap', 'square');
-			});
+		orientation.forEach(function (o) {
+			group.append('path').attr('d', path(o)).attr('stroke-width', strokeWidth).attr('shape-rendering', shapeRendering).attr('stroke', stroke).attr('stroke-linecap', 'square');
+		});
 	};
 
 	$.heavier = function (_) {
@@ -255,7 +212,11 @@ var lines = () => {
 		return $;
 	};
 
-	$.orientation = function (...args) {
+	$.orientation = function () {
+		for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+			args[_key] = arguments[_key];
+		}
+
 		if (arguments.length === 0) {
 			return $;
 		}
@@ -287,74 +248,59 @@ var lines = () => {
 	};
 
 	$.url = function () {
-		return `url(#${id})`;
+		return 'url(#' + id + ')';
 	};
 
 	return $;
-};
+});
 
-var paths = () => {
-	let width = 1;
-	let height = 1;
-	let size = 20;
-	let stroke = '#343434';
-	let strokeWidth = 2;
-	let background = '';
-	let d = s => `M ${s / 4},${s * 3 / 4}l${s / 4},${-s / 2}l${s / 4},${s / 2}`;
-	let id = rand();
-	let fill = 'transparent';
-	let shapeRendering = 'auto';
+var paths = (function () {
+	var width = 1;
+	var height = 1;
+	var size = 20;
+	var stroke = '#343434';
+	var strokeWidth = 2;
+	var background = '';
+	var d = function d(s) {
+		return 'M ' + s / 4 + ',' + s * 3 / 4 + 'l' + s / 4 + ',' + -s / 2 + 'l' + s / 4 + ',' + s / 2;
+	};
+	var id = rand();
+	var fill = 'transparent';
+	var shapeRendering = 'auto';
 
-	const path = _ => {
-		const s = size;
+	var path = function path(_) {
+		var s = size;
 		switch (_) {
 			case 'squares':
-				return `M ${s / 4} ${s / 4} l ${s / 2} 0 l 0 ${s / 2} l ${-s / 2} 0 Z`;
+				return 'M ' + s / 4 + ' ' + s / 4 + ' l ' + s / 2 + ' 0 l 0 ' + s / 2 + ' l ' + -s / 2 + ' 0 Z';
 			case 'nylon':
-				return `M 0 ${s / 4} l ${s / 4} 0 l 0 ${-s / 4} M ${s * 3 / 4} ${s} l 0 ${-s / 4} l ${s / 4} 0 M ${s / 4} ${s / 2} l 0 ${s / 4} l ${s / 4} 0 M ${s / 2} ${s / 4} l ${s / 4} 0 l 0 ${s / 4}`;
+				return 'M 0 ' + s / 4 + ' l ' + s / 4 + ' 0 l 0 ' + -s / 4 + ' M ' + s * 3 / 4 + ' ' + s + ' l 0 ' + -s / 4 + ' l ' + s / 4 + ' 0 M ' + s / 4 + ' ' + s / 2 + ' l 0 ' + s / 4 + ' l ' + s / 4 + ' 0 M ' + s / 2 + ' ' + s / 4 + ' l ' + s / 4 + ' 0 l 0 ' + s / 4;
 			case 'waves':
-				return `M 0 ${s / 2} c ${s / 8} ${-s / 4} , ${s * 3 / 8} ${-s / 4} , ${s / 2} 0 c ${s / 8} ${s / 4} , ${s * 3 / 8} ${s / 4} , ${s / 2} 0 M ${-s / 2} ${s / 2} c ${s / 8} ${s / 4} , ${s * 3 / 8} ${s / 4} , ${s / 2} 0 M ${s} ${s / 2} c ${s / 8} ${-s / 4} , ${s * 3 / 8} ${-s / 4} , ${s / 2} 0`;
+				return 'M 0 ' + s / 2 + ' c ' + s / 8 + ' ' + -s / 4 + ' , ' + s * 3 / 8 + ' ' + -s / 4 + ' , ' + s / 2 + ' 0 c ' + s / 8 + ' ' + s / 4 + ' , ' + s * 3 / 8 + ' ' + s / 4 + ' , ' + s / 2 + ' 0 M ' + -s / 2 + ' ' + s / 2 + ' c ' + s / 8 + ' ' + s / 4 + ' , ' + s * 3 / 8 + ' ' + s / 4 + ' , ' + s / 2 + ' 0 M ' + s + ' ' + s / 2 + ' c ' + s / 8 + ' ' + -s / 4 + ' , ' + s * 3 / 8 + ' ' + -s / 4 + ' , ' + s / 2 + ' 0';
 			case 'woven':
-				return `M ${s / 4},${s / 4}l${s / 2},${s / 2}M${s * 3 / 4},${s / 4}l${s / 2},${-s / 2} M${s / 4},${s * 3 / 4}l${-s / 2},${s / 2}M${s * 3 / 4},${s * 5 / 4}l${s / 2},${-s / 2} M${-s / 4},${s / 4}l${s / 2},${-s / 2}`;
+				return 'M ' + s / 4 + ',' + s / 4 + 'l' + s / 2 + ',' + s / 2 + 'M' + s * 3 / 4 + ',' + s / 4 + 'l' + s / 2 + ',' + -s / 2 + ' M' + s / 4 + ',' + s * 3 / 4 + 'l' + -s / 2 + ',' + s / 2 + 'M' + s * 3 / 4 + ',' + s * 5 / 4 + 'l' + s / 2 + ',' + -s / 2 + ' M' + -s / 4 + ',' + s / 4 + 'l' + s / 2 + ',' + -s / 2;
 			case 'crosses':
-				return `M ${s / 4},${s / 4}l${s / 2},${s / 2}M${s / 4},${s * 3 / 4}l${s / 2},${-s / 2}`;
+				return 'M ' + s / 4 + ',' + s / 4 + 'l' + s / 2 + ',' + s / 2 + 'M' + s / 4 + ',' + s * 3 / 4 + 'l' + s / 2 + ',' + -s / 2;
 			case 'caps':
-				return `M ${s / 4},${s * 3 / 4}l${s / 4},${-s / 2}l${s / 4},${s / 2}`;
+				return 'M ' + s / 4 + ',' + s * 3 / 4 + 'l' + s / 4 + ',' + -s / 2 + 'l' + s / 4 + ',' + s / 2;
 			case 'hexagons':
 				width = 3;
 				height = Math.sqrt(3);
-				return `M ${s},0 l ${s},0 l ${s / 2},${(s * Math.sqrt(3) / 2)} l ${(-s / 2)},${(s * Math.sqrt(3) / 2)} l ${(-s)},0 l ${(-s / 2)},${(-s * Math.sqrt(3) / 2)} Z M 0,${s * Math.sqrt(3) / 2} l ${s / 2},0 M ${(3 * s)},${s * Math.sqrt(3) / 2} l ${(-s / 2)},0`;
+				return 'M ' + s + ',0 l ' + s + ',0 l ' + s / 2 + ',' + s * Math.sqrt(3) / 2 + ' l ' + -s / 2 + ',' + s * Math.sqrt(3) / 2 + ' l ' + -s + ',0 l ' + -s / 2 + ',' + -s * Math.sqrt(3) / 2 + ' Z M 0,' + s * Math.sqrt(3) / 2 + ' l ' + s / 2 + ',0 M ' + 3 * s + ',' + s * Math.sqrt(3) / 2 + ' l ' + -s / 2 + ',0';
 			default:
 				return _(s);
 		}
 	};
 
-	const $ = selection => {
-		const p = path(d);
-		const group = selection
-			.append('defs')
-			.append('pattern')
-				.attr('id', id)
-				.attr('patternUnits', 'userSpaceOnUse')
-				.attr('width', size * width)
-				.attr('height', size * height);
+	var $ = function $(selection) {
+		var p = path(d);
+		var group = selection.append('defs').append('pattern').attr('id', id).attr('patternUnits', 'userSpaceOnUse').attr('width', size * width).attr('height', size * height);
 
 		if (background) {
-			group
-				.append('rect')
-					.attr('width', size * width)
-					.attr('height', size * height)
-					.attr('fill', background);
+			group.append('rect').attr('width', size * width).attr('height', size * height).attr('fill', background);
 		}
 
-		group
-			.append('path')
-				.attr('d', p)
-				.attr('fill', fill)
-				.attr('stroke', stroke)
-				.attr('stroke-width', strokeWidth)
-				.attr('stroke-linecap', 'square')
-				.attr('shape-rendering', shapeRendering);
+		group.append('path').attr('d', p).attr('fill', fill).attr('stroke', stroke).attr('stroke-width', strokeWidth).attr('stroke-linecap', 'square').attr('shape-rendering', shapeRendering);
 	};
 
 	$.heavier = function (_) {
@@ -437,16 +383,16 @@ var paths = () => {
 	};
 
 	$.url = function () {
-		return `url(#${id})`;
+		return 'url(#' + id + ')';
 	};
 
 	return $;
-};
+});
 
 var main = {
-	circles,
-	lines,
-	paths
+	circles: circles,
+	lines: lines,
+	paths: paths
 };
 
 export default main;
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,27 @@
 import uglify from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
 import {minify} from 'uglify-es';
 import pkg from './package.json';
 
 export default [
 	{
 		input: 'src/main.js',
-		plugins: [uglify({}, minify)],
+		plugins: [
+			//uglify({}, minify),
+			babel({
+				exclude: ['node_modules/**']
+			})
+		],
 		output: {file: pkg.main, format: 'umd'},
 		name: 'textures'
 	},
 	{
 		input: 'src/main.js',
-		output: {file: pkg.module, format: 'es'}
+		output: {file: pkg.module, format: 'es'},
+		plugins: [
+			babel({
+				exclude: ['node_modules/**']
+			})
+		],
 	}
 ];
